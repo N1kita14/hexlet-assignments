88eeb85f7a678bf9976bc59175f4c51ecacb97e2
@hexlet/cli <support@hexlet.io>
2025-03-12 19:04:38 +0300

    submit java-oop-ru/classes-and-objects

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
@@ -1,5 +1,21 @@
 package exercise;
 
 // BEGIN
+class Point {
+    final int coordinateX;
+    final int coordinateY;
 
+    Point(int x, int y) {
+        this.coordinateX = x;
+        this.coordinateY = y;
+    }
+
+    public int getX() {
+        return coordinateX;
+    }
+
+    public int getY() {
+        return coordinateY;
+    }
+}
 // END
--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -1,5 +1,27 @@
 package exercise;
 
 // BEGIN
+class Segment {
+    public Point beginPoint;
+    public Point endPoint;
 
+    public Segment(Point beginPoint, Point endPoint) {
+        this.beginPoint = beginPoint;
+        this.endPoint = endPoint;
+    }
+
+    public Point getBeginPoint() {
+        return beginPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public Point getMidPoint() {
+        int x = (beginPoint.getX() + endPoint.getX()) / 2;
+        int y = (beginPoint.getY() + endPoint.getY()) / 2;
+        return new Point(x,y);
+    }
+}
 // END
