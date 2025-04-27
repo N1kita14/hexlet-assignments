a83d3ca9773e3cdedbbcfcf850d2c33637954d10
@hexlet/cli <support@hexlet.io>
2025-04-27 18:57:28 +0300

    submit java-spring-boot-ru/jpa-auditing

--- java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
+++ java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -1,6 +1,9 @@
 package exercise.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
