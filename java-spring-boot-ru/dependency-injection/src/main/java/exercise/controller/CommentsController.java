01cd7f8afe38c4cacb950aab14e5da7f9738a000
@hexlet/cli <support@hexlet.io>
2025-05-04 18:42:35 +0300

    submit java-spring-boot-ru/dependency-injection

---
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -0,0 +1,59 @@
+package exercise.controller;
+
+//import exercise.model.Post;
+//import exercise.repository.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+//import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<Comment> getListComments() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Comment show(@PathVariable long id) {
+        return commentRepository.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment create(@RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @PutMapping
+    public Comment update(@PathVariable long id, @RequestBody Comment comment) {
+        var comment11 = commentRepository.findById(id).get();
+        comment11.setPostId(comment.getPostId());
+        comment11.setBody(comment.getBody());
+        commentRepository.save(comment11);
+        return comment;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        commentRepository.deleteById(id);
+    }
+}
---
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,60 @@
+package exercise.controller;
+
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+//import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<Post> getListPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post show(@PathVariable long id) {
+        return postRepository.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping
+    public Post update(@PathVariable long id, @RequestBody Post post) {
+        var post11 = postRepository.findById(id).get();
+        post11.setTitle(post.getTitle());
+        post11.setBody(post.getBody());
+        postRepository.save(post11);
+        return post;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        commentRepository.deleteByPostId(id);
+        postRepository.deleteById(id);
+    }
+}
