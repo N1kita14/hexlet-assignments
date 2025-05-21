77ee39f8a9f775663996f05ae5d1cf11f8741f7c
@hexlet/cli <support@hexlet.io>
2025-05-21 18:52:58 +0300

    submit java-spring-boot-ru/dto

--- java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -1,54 +1,56 @@
 package exercise.controller;
 
-//import exercise.repository.CommentRepository;
-
+import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 
 import exercise.model.Post;
 import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 import exercise.dto.PostDTO;
-//import exercise.dto.CommentDTO;
+import exercise.dto.CommentDTO;
 
+// BEGIN
 @RestController
 @RequestMapping("/posts")
 public class PostsController {
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-    @GetMapping
+    @GetMapping(path = "")
     public List<PostDTO> index() {
-        var posts = postRepository.findAll();
-        var result = posts.stream()
+        return postRepository.findAll().stream()
                 .map(this::toDTO)
                 .toList();
-        return result;
     }
-
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}")
     public PostDTO show(@PathVariable Long id) {
         var post = postRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
-
-        var dto = new PostDTO();
-        dto.setId(post.getId());
-        dto.setBody(post.getBody());
-        dto.setTitle(post.getTitle());
-        return dto;
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        return toDTO(post);
     }
 
     private PostDTO toDTO(Post post) {
         var dto = new PostDTO();
         dto.setId(post.getId());
-        dto.setBody(post.getBody());
         dto.setTitle(post.getTitle());
-
+        dto.setBody(post.getBody());
+        List<CommentDTO> comments = commentRepository.findByPostId(post.getId()).stream()
+                .map(comment -> {
+                    CommentDTO commentDTO = new CommentDTO();
+                    commentDTO.setId(comment.getId());
+                    commentDTO.setBody(comment.getBody());
+                    return commentDTO;
+                })
+                .toList();
+        dto.setComments(comments);
         return dto;
     }
 }
+// END
