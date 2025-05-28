098b91cd880e001e5e4dba80a87976f2ace3ccfb
@hexlet/cli <support@hexlet.io>
2025-05-28 20:06:12 +0300

    submit java-spring-boot-ru/partial-update

--- java-spring-boot-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-boot-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -1,14 +1,15 @@
 package exercise.mapper;
 
+import exercise.dto.CarCreateDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
-//import exercise.dto.CarCreateDTO;
+import exercise.dto.CarCreateDTO;
 import exercise.dto.CarUpdateDTO;
-//import exercise.dto.CarDTO;
+import exercise.dto.CarDTO;
 import exercise.model.Car;
 
 // BEGIN
@@ -19,7 +20,8 @@ import exercise.model.Car;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class CarMapper {
-
+    public abstract Car map(CarCreateDTO dto);
+    public abstract CarDTO map(Car model);
     public abstract void update(CarUpdateDTO dto, @MappingTarget Car model);
 }
 // END
